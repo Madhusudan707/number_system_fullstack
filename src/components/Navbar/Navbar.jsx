ef1fe53a66a5899b508f4e206a1a7ddfd8db954b
xdev200 <xdev200200@gmail.com>
2021-02-04 19:54:40 +0530

    Added: Github link to navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -48,7 +48,7 @@ const useStyles = makeStyles((theme) => ({
           <ul>
             <li><a href=''>Home</a></li>
             <li><a href=''>NumberFact</a></li>
-            <li><a href='https://github.com/Madhusudan707/number_system_fullstack'>GitHub</a></li>
+            <li><a href='https://github.com/Madhusudan707/number_system_fullstack' target='_blank'>GitHub</a></li>
           </ul>
         </Toolbar>
       </AppBar>
