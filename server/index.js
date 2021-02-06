f9006509ed52716191369eee2d05b84492adb744
xdev200 <xdev200200@gmail.com>
2021-02-06 13:41:58 +0530

    optimized: validation

--- server/index.js
+++ server/index.js
@@ -1,25 +1,26 @@
 const express = require("express");
-const path = require('path');
+const path = require("path");
 const cors = require("cors");
 const app = express();
 const router = express.Router();
 const convert = require("./convert");
-const validation = require('./validation')
+const validation = require("./validation");
 const body_parser = require("body-parser");
 
 const port = process.env.PORT || 3000;
-const DIST_DIR = path.join(__dirname, '../dist'); // NEW
-const HTML_FILE = path.join(DIST_DIR, 'index.html'); // NEW
+const DIST_DIR = path.join(__dirname, "../dist"); // NEW
+const HTML_FILE = path.join(DIST_DIR, "index.html"); // NEW
 
 app.use(cors());
 app.use(express.static("../public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(body_parser.json());
-app.use(express.static(DIST_DIR)); 
+app.use(express.static(DIST_DIR));
+
 //Home
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(HTML_FILE); // EDIT
- });
+});
 
 //Number's Base
 
@@ -31,7 +32,7 @@ const base2 = 2,
 
 app.get("/B2ALL/:binary", (req, res) => {
   let binary = req.params.binary;
-  let validateInput = validation.isBinary(binary)
+  let validateInput = validation.isValidNumber(binary, "b"); //"b":binary
   if (binary && validateInput) {
     res.send({
       resultB2D: convert.BinaryToDecimal(binary, base2),
@@ -42,7 +43,7 @@ app.get("/B2ALL/:binary", (req, res) => {
       ),
     });
   } else {
-   res.send({Error:"Invalid Binary Number"})
+    res.send({ Error: "Invalid Binary Number" });
   }
 });
 
@@ -50,15 +51,15 @@ app.get("/B2ALL/:binary", (req, res) => {
 
 app.get("/D2ALL/:decimal", (req, res) => {
   let decimal = req.params.decimal;
-  let validateInput = validation.isDecimal(decimal)
-  if (decimal && validateInput ) {
+  let validateInput = validation.isDecimal(decimal, "d"); //"d":decimal
+  if (decimal && validateInput) {
     res.send({
       resultD2B: convert.DecimalToBinary_Octal_hexaDecimal(decimal, base2),
       resultD2O: convert.DecimalToBinary_Octal_hexaDecimal(decimal, base8),
       resultD2HD: convert.DecimalToBinary_Octal_hexaDecimal(decimal, base16),
     });
   } else {
-    res.send({Error:"Invalid Decimal Number"})
+    res.send({ Error: "Invalid Decimal Number" });
   }
 });
 
@@ -66,7 +67,7 @@ app.get("/D2ALL/:decimal", (req, res) => {
 
 app.get("/O2ALL/:octal", (req, res, next) => {
   let octal = req.params.octal;
-  let validateInput = validation.isOctal(octal)
+  let validateInput = validation.isOctal(octal, "o"); //"o":octal
   if (octal && validateInput) {
     res.send({
       resultO2B: convert.DecimalToBinary_Octal_hexaDecimal(
@@ -80,7 +81,7 @@ app.get("/O2ALL/:octal", (req, res, next) => {
       ),
     });
   } else {
-    res.send({Error:"Invalid Octal Number"})
+    res.send({ Error: "Invalid Octal Number" });
   }
 });
 
@@ -88,7 +89,7 @@ app.get("/O2ALL/:octal", (req, res, next) => {
 
 app.get("/HD2ALL/:hexadecimal", (req, res, next) => {
   let hexadecimal = req.params.hexadecimal;
-  let validateInput = validation.isHexaDecimal(hexadecimal)
+  let validateInput = validation.isHexaDecimal(hexadecimal, "hd"); ////"hd":hexadecimal
   if (hexadecimal && validateInput) {
     res.send({
       resultHD2B: convert.DecimalToBinary_Octal_hexaDecimal(
@@ -102,12 +103,12 @@ app.get("/HD2ALL/:hexadecimal", (req, res, next) => {
       ),
     });
   } else {
-    res.send({Error:"Invalid HexaDecimal Number"})
+    res.send({ Error: "Invalid HexaDecimal Number" });
   }
 });
 
 app.listen(port, function () {
-  console.log('App listening on port: ' + port);
- });
+  console.log("App listening on port: " + port);
+});
 
 module.exports = app;
--- server/validation.js
+++ server/validation.js
@@ -1,112 +1,66 @@
+// 1.isValidNumber -Check Input is valid Binary, Decimal, Octal or HexaDecimal
 
-
-  //1. Switching Functions
-//   const switchNs = (input,push) =>{
-//       switch(push){
-//           case "b":
-
-//               return isBinary(input)
-
-//       }
-//   }
-
-
-//   let arr,
-//   length,
-//   flag = false;
-
-  //A Universal Function
-
-//   const universalValidation = (input,s) =>{
-//     let arr,length,flag
-
-//     arr = input.split("")
-//     length = arr.length;
-//     for(let i =0 ; i <length;i++)
-//     switch(s){
-//         case "b":
-
-//     }
-//   }
-//1 Binary check
-const isBinary = (input) => {
-  arr = input.split("");
-  length = arr.length;
+const isValidNumber = (input, push) => {
+  let arr = input.split("");
+  let length = arr.length;
+  let flag = false;
 
   for (let i = 0; i < length; i++) {
-    if (arr[i] > 1 || isNaN(arr[i])) {
-      flag = false;
-      break;
-    } else {
-      flag = true;
-    }
-  }
-  return flag;
-};
-
-//2 Decimal Check
-
-const isDecimal = (input) => {
-  arr = input.split("");
-  length = arr.length;
-
-  for (let i = 0; i < length; i++) {
-    if (isNaN(arr[i])) {
-      flag = false;
-      console.log(arr[i]);
-      break;
-    } else {
-      console.log(arr[i]);
-      flag = true;
-    }
-  }
-  return flag;
-};
-
-//Octal Check
-
-const isOctal = (input) => {
-  arr = input.split("");
-  length = arr.length;
-
-  for (let i = 0; i < length; i++) {
-    if (arr[i] > 7 || isNaN(arr[i])) {
-      flag = false;
-      console.log(arr[i]);
-      break;
-    } else {
-      console.log(arr[i]);
-      flag = true;
-    }
-  }
-  return flag;
-};
-
-//4 HexaDecimal Check
-
-const isHexaDecimal = (input) => {
-  arr = input.split("");
-  length = arr.length;
-
-  for (let i = 0; i < length; i++) {
-    if (!isA2FCheck(arr[i])) {
-      console.log("yes");
-      if (arr[i] < 0 || isNaN(arr[i])) {
-        console.log("yes");
-        flag = false;
+    switch (push) {
+      case "b": //"b":binary
+        if (arr[i] > 1 || isNaN(arr[i])) {
+          flag = false;
+          break;
+        } else {
+          flag = true;
+        }
         break;
-      }else{
-        flag = true;
-      }
-    } else {
-      flag = true;
+
+      case "d": //"d":decimal
+        if (isNaN(arr[i])) {
+          flag = false;
+          console.log(arr[i]);
+          break;
+        } else {
+          console.log(arr[i]);
+          flag = true;
+        }
+        break;
+
+      case "o": //"o":octal
+        if (arr[i] > 7 || isNaN(arr[i])) {
+          flag = false;
+          console.log(arr[i]);
+          break;
+        } else {
+          console.log(arr[i]);
+          flag = true;
+        }
+        break;
+
+      case "hd": //"hd":hexadecimal
+        if (!isA2FCheck(arr[i])) {
+          console.log("yes");
+          if (arr[i] < 0 || isNaN(arr[i])) {
+            console.log("yes");
+            flag = false;
+            break;
+          } else {
+            flag = true;
+          }
+        } else {
+          flag = true;
+        }
+        break;
+
+      default:
+        return false;
     }
   }
-
   return flag;
 };
 
-//5 Alphabet check for HexaDecimal
+//2 Alphabet check for HexaDecimal - Check HexaDecimal contain Valid Alphabet Number
 
 const isA2FCheck = (alphabet) => {
   switch (alphabet) {
@@ -128,4 +82,5 @@ const isA2FCheck = (alphabet) => {
       return false;
   }
 };
-module.exports = { isBinary, isDecimal, isOctal, isHexaDecimal };
+
+module.exports = { isValidNumber };
