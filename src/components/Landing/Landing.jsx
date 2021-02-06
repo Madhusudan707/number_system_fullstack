ad5f0ff368e3d997a6fb836e2fa419ed493e8020
xdev200 <xdev200200@gmail.com>
2021-02-06 10:18:56 +0530

    Fixed: Output Text of Labels

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -24,7 +24,6 @@ const Landing = () => {
   let input, id, inputBox;
 
   function clickHandler() {
-  
     inputBox = document.querySelectorAll(".MuiInputBase-input");
     for (let i = 0; i < inputBox.length; i++) {
       let textBoxValue = 0;
@@ -32,45 +31,63 @@ const Landing = () => {
       if (i === 0 && textBoxValue) {
         //Binary to All
         //API is using in the same domain so absolute path is not necessary
-          axios.get(`/B2ALL/${textBoxValue}`).then((res) => {
+        axios.get(`/B2ALL/${textBoxValue}`).then((res) => {
           document.getElementById("decimal").value = res.data.resultB2D;
           document.getElementById("octal").value = res.data.resultB2O;
           document.getElementById("hexadecimal").value = res.data.resultB2HD;
+
+          document.getElementById("decimal-label").innerHTML = "Decimal"
+          document.getElementById("octal-label").innerHTML = "Octal"
+          document.getElementById("hexadecimal-label").innerHTML = "HexaDecimal"
         });
       } else if (i === 1 && textBoxValue) {
-       //Decimal to All
-       axios.get(`/D2ALL/${textBoxValue}`).then((res) => {
-        document.getElementById("binary").value = res.data.resultD2B;
-        document.getElementById("octal").value = res.data.resultD2O;
-        document.getElementById("hexadecimal").value = res.data.resultD2HD;
-      });
+        //Decimal to All
+        axios.get(`/D2ALL/${textBoxValue}`).then((res) => {
+          document.getElementById("binary").value = res.data.resultD2B;
+          document.getElementById("octal").value = res.data.resultD2O;
+          document.getElementById("hexadecimal").value = res.data.resultD2HD;
+
+          document.getElementById("binary-label").innerHTML = "Binary"
+          document.getElementById("octal-label").innerHTML = "Octal"
+          document.getElementById("hexadecimal-label").innerHTML = "HexaDecimal"
+        });
       } else if (i === 2 && textBoxValue) {
         //Octal to All
         axios.get(`/O2ALL/${textBoxValue}`).then((res) => {
-        document.getElementById("binary").value = res.data.resultO2B;
-        document.getElementById("decimal").value = res.data.resultO2D;
-        document.getElementById("hexadecimal").value = res.data.resultO2HD;
-      });
-       
+          document.getElementById("binary").value = res.data.resultO2B;
+          document.getElementById("decimal").value = res.data.resultO2D;
+          document.getElementById("hexadecimal").value = res.data.resultO2HD;
+
+          document.getElementById("binary-label").innerHTML = "Binary"
+          document.getElementById("decimal-label").innerHTML = "Decimal"
+          document.getElementById("hexadecimal-label").innerHTML = "HexaDecimal"
+          
+        });
       } else if (i === 3 && textBoxValue) {
         //HexaDecimal to All
         axios.get(`/HD2ALL/${textBoxValue}`).then((res) => {
-        document.getElementById("binary").value = res.data.resultHD2B;
-        document.getElementById("decimal").value = res.data.resultHD2D;
-        document.getElementById("octal").value = res.data.resultHD2O;
-      });
-       
+          document.getElementById("binary").value = res.data.resultHD2B;
+          document.getElementById("decimal").value = res.data.resultHD2D;
+          document.getElementById("octal").value = res.data.resultHD2O;
+
+          document.getElementById("binary-label").innerHTML = "Binary"
+          document.getElementById("decimal-label").innerHTML = "Decimal"
+          document.getElementById("octal-label").innerHTML = "HexaDecimal"
+         
+        });
       }
-   }
-   
-  
-   
+    }
   }
   function resetHandler(e) {
     document.getElementById("binary").value = "";
     document.getElementById("decimal").value = "";
     document.getElementById("octal").value = "";
     document.getElementById("hexadecimal").value = "";
+
+    document.getElementById("binary-label").innerHTML = "Enter Binary"
+    document.getElementById("decimal-label").innerHTML = "Enter Decimal"
+    document.getElementById("octal-label").innerHTML = "Enter Octal"
+    document.getElementById("hexadecimal-label").innerHTML = "Enter HexaDecimal"
   }
 
   function inputHandler(event) {
@@ -90,39 +107,34 @@ const Landing = () => {
               value=" "
               id="binary"
               className="nsBox"
-              placeholder="Enter Binary"
               inputHandler={inputHandler}
-              label='Enter Binary'
+              label="Enter Binary"
             />
             <InputBox
               value=" "
               id="decimal"
               className="nsBox"
-              placeholder="Enter Decimal"
               inputHandler={inputHandler}
-              label='Enter Decimal'
-              
+              label="Enter Decimal"
             />
-            
+
             <InputBox
               value=" "
               id="octal"
               className="nsBox"
-              placeholder="Enter Octal"
               inputHandler={inputHandler}
-              label='Enter Octal'
+              label="Enter Octal"
             />
             <InputBox
               value=" "
               id="hexadecimal"
               className="nsBox"
-              placeholder="Enter Hexa Decimal"
               inputHandler={inputHandler}
-              label='Enter HexaDecimal'
+              label="Enter HexaDecimal"
             />
           </div>
         </Grid>
-      <p>{inputData}</p>
+        <p>{inputData}</p>
         <ContainedButtons name="SUBMIT" Handler={clickHandler} />
         <ContainedButtons name="RESET" Handler={resetHandler} />
       </Grid>
